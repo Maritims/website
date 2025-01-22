5ff6d4346553cb4f76f9346fbec5b95a076d71bc
Martin Severin Steffensen <martin@cosmosit.no>
2025-01-22 23:12:04 +0100

    Created static site generator script.

---
+++ build.js
@@ -0,0 +1,66 @@
+import { format } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+import ejs from 'ejs';
+import fs from 'fs';
+
+const buildDir = 'build';
+const srcDir = 'src';
+const staticDir = 'static';
+const buildDateTime = formatInTimeZone(new Date(), 'Europe/Oslo', 'yyyy-MM-dd HH:mm:ss zzz');
+
+/**
+ * @typedef {object} Page
+ * @property {string} title
+ * @property {string} description
+ * @property {string} filename
+ */
+
+/**
+ * @typedef {object} Page
+ * @property {string} title
+ * @property {string} description
+ * @property {string} htmlContent
+ */
+
+/**
+ * @type {Page[]}
+ */
+const pages = [{
+    title: 'Martin Severin Steffensen',
+    description: 'My personal website.',
+    filename: 'index.html'
+}, {
+    title: 'EVE Online: Ninja Hacking Guide',
+    description: 'A comprehensive guide to ninja hacking data and relic sites in C5 wormholes using a Myrmidon. Includes fit, tactics, and site-specific strategies.',
+    filename: 'eve-online-ninja-hacking-guide.html'
+}];
+
+/**
+ * @type {string}
+ */
+const template = fs.readFileSync(`${srcDir}/layout.html`, 'utf-8');
+
+pages.forEach(page => {
+    console.log(`Building ${page.filename}`);
+
+    const rawHtml = fs.readFileSync(`${srcDir}/${page.filename}`);
+    const renderedHtml = ejs.render(template, {
+        ...page,
+        htmlContent: rawHtml,
+        lastUpdated: buildDateTime
+    });
+
+    fs.writeFileSync(`${buildDir}/${page.filename}`, renderedHtml);
+});
+
+/**
+ * @type {string[]}
+ */
+const staticFiles = fs.readdirSync(staticDir);
+
+staticFiles.forEach(staticFile => {
+    fs.copyFileSync(`${staticDir}/${staticFile}`, `${buildDir}/${staticFile}`);
+});
+
+
+console.log(`Build complete`);
