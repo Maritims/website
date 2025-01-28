fa48e2d730b2279922d6ddfa25ee6fe592eca702
Martin Severin Steffensen <martin@cosmosit.no>
2025-01-28 22:54:50 +0100

    Implemented gallery.

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
-import { format } from 'date-fns';
 import { formatInTimeZone } from 'date-fns-tz';
-import ejs from 'ejs';
+import ejs, { render } from 'ejs';
 import fs from 'fs';
 
+const now = new Date();
 const buildDir = 'build';
 const srcDir = 'src';
 const staticDir = 'static';
@@ -12,7 +12,7 @@ const buildDateTime = formatInTimeZone(new Date(), 'Europe/Oslo', 'yyyy-MM-dd HH
  * @typedef {object} Page
  * @property {string} title
  * @property {string} description
- * @property {string} filename
+ * @property {string} name
  */
 
 /**
@@ -21,29 +21,52 @@ const buildDateTime = formatInTimeZone(new Date(), 'Europe/Oslo', 'yyyy-MM-dd HH
 const pages = [{
     title: 'Martin Severin Steffensen',
     description: 'My personal website.',
-    filename: 'index.html'
+    name: 'index'
 }, {
     title: 'EVE Online: Ninja Hacking Guide',
     description: 'A comprehensive guide to ninja hacking data and relic sites in C5 wormholes using a Myrmidon. Includes fit, tactics, and site-specific strategies.',
-    filename: 'eve-online-ninja-hacking-guide.html'
+    name: 'eve-online-ninja-hacking-guide'
+}, {
+    title: 'Gallery',
+    description: 'This used to be my Instagram feed.',
+    name: 'gallery'
 }];
 
-/**
- * @type {string}
- */
-const template = fs.readFileSync(`${srcDir}/layout.html`, 'utf-8');
+const templateHtml = fs.readFileSync(`${srcDir}/layout.html`, 'utf-8');
+const templateFileLastModified = fs.statSync(`${srcDir}/layout.html`).mtimeMs;
 
 pages.forEach(page => {
-    console.log(`Building ${page.filename}`);
+    // Should we build the page?
+    // We should build it if the template is newer than the page.
 
-    const rawHtml = fs.readFileSync(`${srcDir}/${page.filename}`);
-    const renderedHtml = ejs.render(template, {
+    const sourceHtmlFile = `${srcDir}/${page.name}.html`;
+    const targetHtmlFile = `${buildDir}/${page.name}.html`;
+    if(fs.existsSync(targetHtmlFile)) {
+        const sourceFileLastModified = fs.statSync(sourceHtmlFile).mtimeMs;
+        const targetFileLastModified = fs.statSync(targetHtmlFile).mtimeMs;
+        if(targetFileLastModified > sourceFileLastModified && targetFileLastModified > templateFileLastModified) {
+            console.log(`Skipping ${page.name}`);
+            return;
+        }
+    }
+
+    console.log(`Building ${page.name}`);
+
+    const rawHtml = fs.readFileSync(`${srcDir}/${page.name}.html`);
+    const renderOptions = {
         ...page,
         htmlContent: rawHtml,
+        scripts: [],
         lastUpdated: buildDateTime
-    });
+    };
 
-    fs.writeFileSync(`${buildDir}/${page.filename}`, renderedHtml);
+    const hasScriptFile = fs.existsSync(`${staticDir}/${page.name}.js`)
+    if(hasScriptFile) {
+        renderOptions.scripts.push(`${page.name}.js`);
+    }
+    const renderedHtml = ejs.render(templateHtml, renderOptions);
+
+    fs.writeFileSync(`${buildDir}/${page.name}.html`, renderedHtml);
 });
 
 /**
@@ -52,8 +75,20 @@ pages.forEach(page => {
 const staticFiles = fs.readdirSync(staticDir);
 
 staticFiles.forEach(staticFile => {
-    fs.copyFileSync(`${staticDir}/${staticFile}`, `${buildDir}/${staticFile}`);
+    const sourcePath = `${staticDir}/${staticFile}`;
+    const sourceStats = fs.statSync(sourcePath);
+    const targetPath = `${buildDir}/${staticFile}`
+
+    console.log(`Copying ${sourcePath} to ${targetPath}`);
+    
+    if(sourceStats.isDirectory()) {
+        fs.cpSync(sourcePath, targetPath, {
+            recursive: true
+        });
+    } else {
+        fs.copyFileSync(`${sourcePath}`, `${targetPath}`);
+    }
 });
 
-
-console.log(`Build complete`);
+const elapsed = new Date().getTime() - now;
+console.log(`Build complete in ${elapsed / 1000} s`);
