84b7e53602d298f0be25489c3668df511e0c2627
maritims <maritim@gmail.com>
2026-01-19 23:27:37 +0100

    Hacker theme.

--- website/static/theme-switcher.js
+++ website/static/theme-switcher.js
@@ -10,33 +10,15 @@ class ThemeSwitcher extends HTMLElement {
      * @private
      */
     _themes = [{
-       label: "Default",
-       value: 'default'
+        label: "Default",
+        value: 'default'
+    }, {
+        label: "Hacker",
+        value: 'hacker'
     }, {
         label: "LCARS",
         value: 'lcars'
     }];
-    // noinspection JSUnusedGlobalSymbols
-    _presets = {
-        "hacker": {
-            label: "Hacker",
-            values: {
-                "--font-family": "monospace",
-                "--first-foreground-color": "#00ff00",
-                "--first-background-color": "#000000",
-                "--second-background-color": "#0a0a0a",
-                "--link-color": "#00ff00",
-                "--link-color-visited": "#008800",
-                "--link-color-hover": "#00aa00",
-                "--button-border-width": "1px",
-                "--button-border-style": "solid",
-                "--button-border-color": "#008800",
-                "--button-background-color": "#000000",
-                "--button-foreground-color": "#00FF00",
-                "--button-hover-background-color": "#008800",
-            }
-        }
-    };
 
     constructor() {
         super();
@@ -47,18 +29,8 @@ class ThemeSwitcher extends HTMLElement {
         this.render();
     }
 
-    /**
-     * Render the theme editor dialogue.
-     * @returns {void}
-     */
     render() {
-        const style = ``;
-
-        this.innerHTML = `
-            <style>${style}</style>
-            <select>${this._themes.map(({label, value}) => `<option value="${value}" ${document.documentElement.classList.contains(value) ? 'selected' : ''}>${label}</option>`).join('')}</select>
-        `;
-
+        this.innerHTML = `<select>${this._themes.map(({label, value}) => `<option value="${value}" ${document.documentElement.classList.contains(value) ? 'selected' : ''}>${label}</option>`).join('')}</select>`;
         this.querySelector('select').addEventListener('change', event => {
             this._themes.forEach(theme => document.documentElement.classList.remove(theme.value));
             document.documentElement.classList.add(event.target.value);
