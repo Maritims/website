81ad8b1f388280246e60bccdd60ee622bd45e16e
maritims <maritim@gmail.com>
2026-02-03 20:54:50 +0100

    Configured GraalVM native build.

--- webmention-parent/webmention-cli/src/main/java/no/clueless/webmention_cli/Application.java
+++ webmention-parent/webmention-cli/src/main/java/no/clueless/webmention_cli/Application.java
@@ -1,29 +1,49 @@
 package no.clueless.webmention_cli;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.time.Instant;
+import java.util.Properties;
 
 public class Application {
+    private static final String  ARTIFACT_ID;
+    private static final String  VERSION;
+    private static final Instant BUILD_TIME;
+
+    static {
+        var props = new Properties();
+        try (var inputStream = Application.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (inputStream == null) {
+                throw new RuntimeException("Failed to load application.properties. The file was not found");
+            }
+            props.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load application.properties", e);
+        }
+
+        ARTIFACT_ID = props.getProperty("artifact.id");
+        VERSION     = props.getProperty("version");
+        BUILD_TIME  = Instant.parse(props.getProperty("build.time"));
+    }
+
     private static void printUsage() {
-        System.out.println("""
-                Usage: webmention-cli [options]
+        System.out.printf("""
+                Usage: %s [options]
                 Options:
-                  -u, --uri <uri>    The base URI to use
-                  -d, --dir <path>   The root directory to scan
-                  --dry-run          Show what would happen without sending
-                  -h, --help         Show this help message
-                """);
+                  -u,   --uri <uri>     The base URI to use
+                  -d,   --dir <path>    The root directory to scan
+                  -dr,  --dry-run       Show what would happen without sending
+                  -v,   --version       Show the version
+                  -h,   --help          Show this help message
+                %n""", ARTIFACT_ID);
     }
 
     public static void main(String[] args) {
-        String  uriStr = null;
-        String  dirStr = null;
-        boolean dryRun = false;
+        String uriStr = null;
+        String dirStr = null;
+        var    dryRun = false;
 
         for (var i = 0; i < args.length; i++) {
             switch (args[i]) {
@@ -37,7 +57,11 @@ public class Application {
                         dirStr = args[i];
                     }
                 }
-                case "--dry-run" -> dryRun = true;
+                case "--dry-run", "-dr" -> dryRun = true;
+                case "--version", "-v" -> {
+                    System.out.println(ARTIFACT_ID + " " + VERSION + " (" + BUILD_TIME + ")");
+                    return;
+                }
                 case "--help", "-h" -> {
                     printUsage();
                     return;
